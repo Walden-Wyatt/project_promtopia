de22d1b3da450812d59df91c4c5cf893dd9a7277
unknown <investob848@gmail.com>
2023-12-07 02:10:37 -0800

    fix typo

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -69,7 +69,7 @@ export const DELETE = async ( request, {params}) => {
     try {
         await connectToDB();
 
-        await Prompt.findByIdAndRemove(params.id);
+        await Prompt.findByIdAndDelete(params.id);
 
         return new Response("Prompt deleted successfully", { status: 200 } )
     }
